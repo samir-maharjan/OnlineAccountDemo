f5e51e27381e1b0a46ae91990e3ac733f7afae40
SamirMaharjan2022 <mail.samirmaharjan@gmail.com>
2024-01-30 01:46:33 +0545

    Added Template and updates on Views

--- OnlineAccountDemo/Controllers/UsersController.cs
+++ OnlineAccountDemo/Controllers/UsersController.cs
@@ -25,45 +25,63 @@ namespace OnlineAccountDemo.Controllers
         [HttpPost]
         public IActionResult Create(Users users)
         {
-
-            Users newData = new Users();
-            newData.Name = users.Name;
-            newData.Address = users.Address;
-            newData.Email = users.Email;
-            newData.Password = HashPassword(users.Password);
-            Random random = new Random();
-
-            char[] digits = new char[10];
-            for (int i = 0; i < 10; i++)
+            try
             {
-                digits[i] = (char)('0' + random.Next(10));
+                Users newData = new Users();
+                newData.Name = users.Name;
+                newData.Address = users.Address;
+                newData.Email = users.Email;
+                newData.Password = HashPassword(users.Password);
+                Random random = new Random();
+
+                char[] digits = new char[10];
+                for (int i = 0; i < 10; i++)
+                {
+                    digits[i] = (char)('0' + random.Next(10));
+                }
+
+                newData.UsersAccount = new UsersAccount()
+                {
+                    AccountNumber = new string(digits),
+                    TotalBalance = 0
+
+                };
+                _db.Users.Add(newData);
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Login");
+
+
             }
-
-            newData.UsersAccount = new UsersAccount()
+            catch (Exception ex)
             {
-                AccountNumber = new string(digits),
-                TotalBalance = 0
+                return RedirectToAction("Index", "Login");
+             }
 
-            };
-            _db.Users.Add(newData);
-            _db.SaveChanges();
 
-            return RedirectToAction("Index", "Login");
+
         }
         public string HashPassword(string password)
         {
-            using (SHA256 sha256 = SHA256.Create())
+            try
             {
-                byte[] bytes = Encoding.UTF8.GetBytes(password);
-                byte[] hashBytes = sha256.ComputeHash(bytes);
-                StringBuilder builder = new StringBuilder();
-                foreach (byte b in hashBytes)
+                using (SHA256 sha256 = SHA256.Create())
                 {
-                    builder.Append(b.ToString("x2"));
-                }
+                    byte[] bytes = Encoding.UTF8.GetBytes(password);
+                    byte[] hashBytes = sha256.ComputeHash(bytes);
+                    StringBuilder builder = new StringBuilder();
+                    foreach (byte b in hashBytes)
+                    {
+                        builder.Append(b.ToString("x2"));
+                    }
 
-                return builder.ToString();
+                    return builder.ToString();
+                }
             }
+            catch(Exception ex)
+            {
+                return "Issue faced During Creating Password!";
+            }
+            
         }
     }
 }
